a69fb9d40a45b2d8dd7cc802a0bddf951b40acdf
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-03-07 11:33:25 +0300

    Created domain project

---
+++ src/ShortUrl.Domain/Entities/ShortLink.cs
@@ -0,0 +1,14 @@
+﻿namespace ShortUrl.Domain.Entities;
+
+public class ShortLink {
+    public long Id { get; }
+    public string OriginalUrl { get; }
+    public string Token { get; }
+
+    public ShortLink(long id, string originalUrl, string token)
+    {
+        Id = id;
+        OriginalUrl = originalUrl;
+        Token = token;
+    }
+}
