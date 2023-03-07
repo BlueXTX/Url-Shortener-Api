da99877d24a23aaed9463153bc6430eb4d92371e
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-03-07 17:01:39 +0300

    Defined IFileStorageInterface

---
+++ src/ShortUrl.Application/Interfaces/IFileStorage.cs
@@ -0,0 +1,7 @@
+﻿namespace ShortUrl.Application.Interfaces;
+
+public interface IFileStorage {
+    Task Write(string fileName, Stream data);
+    Task<Stream> Read(string fileName);
+    Task Delete(string fileName);
+}
---
+++ src/ShortUrl.Infrastructure/Services/LocalDriveFileStorage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using ShortUrl.Application.Interfaces;
+
+namespace ShortUrl.Infrastructure.Services;
+
+public class LocalDriveFileStorage : IFileStorage {
+
+    private readonly LocalDriveFileStorageOptions _options;
+
+    public LocalDriveFileStorage(IOptionsSnapshot<LocalDriveFileStorageOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public Task Write(string fileName, Stream data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> Read(string fileName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(string fileName)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public record LocalDriveFileStorageOptions(string BasePath, bool UseRelativePath = true);
