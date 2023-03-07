355afaeaa3ea75a4ee6e3269aaf7fde17c5e3821
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-03-07 12:43:51 +0300

    Implemented Base62NumberEncoder

--- src/ShortUrl.Infrastructure/Services/Base62NumberEncoder.cs
+++ src/ShortUrl.Infrastructure/Services/Base62NumberEncoder.cs
@@ -1,16 +1,36 @@
-﻿using ShortUrl.Application.Interfaces;
+﻿using System.Text;
+using ShortUrl.Application.Interfaces;
 
 namespace ShortUrl.Infrastructure.Services;
 
 public class Base62NumberEncoder : INumberEncoder {
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private const int Base = 62;
 
     public string Encode(int number)
     {
-        throw new NotImplementedException();
+        var stringBuilder = new StringBuilder();
+
+        while (number > 0)
+        {
+            stringBuilder.Append(Alphabet[number % Base]);
+            number /= Base;
+        }
+
+        return new string(stringBuilder.ToString().Reverse().ToArray());
     }
 
     public int Decode(string value)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Value can't be null or empty");
+
+        int number = 0;
+
+        foreach (char ch in value)
+        {
+            number = number * Base + Alphabet.IndexOf(ch);
+        }
+
+        return number;
     }
 }
