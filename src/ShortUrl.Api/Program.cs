e2aa7556cb25fd99ac10f2a871f7e4b8278e6913
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-03-08 00:13:15 +0300

    Program.cs refactor

--- src/ShortUrl.Api/Program.cs
+++ src/ShortUrl.Api/Program.cs
@@ -1,6 +1,4 @@
 using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using ShortUrl.Api.Configuration;
 using ShortUrl.Api.Validators;
 using ShortUrl.Infrastructure;
@@ -10,23 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddShortUrl(builder.Configuration);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateShortLinkDtoValidator>();
 builder.Services.AddControllers();
-
-builder.Services.AddApiVersioning(options => {
-    options.ApiVersionReader = new UrlSegmentApiVersionReader();
-    options.ReportApiVersions = true;
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.UseApiBehavior = true;
-    options.AssumeDefaultVersionWhenUnspecified = true;
-});
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddVersionedApiExplorer(options => {
-    options.GroupNameFormat = "'v'VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
-
-builder.Services.AddSwaggerGen();
-builder.Services.ConfigureOptions<SwaggerOptionsConfigurator>();
+builder.Services.ConfigureSwagger();
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddResponseCaching();
