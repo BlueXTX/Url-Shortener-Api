49ee1408b6f2c901626d2b1b01e4d643df21de31
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-03-08 00:16:43 +0300

    Updated integration test paths

--- tests/ShortUrl.IntegrationTests/Controllers/v1/TokensControllerTests.cs
+++ tests/ShortUrl.IntegrationTests/Controllers/v1/TokensControllerTests.cs
@@ -1,16 +1,13 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
-using ShortUrl.Api;
 using ShortUrl.Api.Dto;
 using ShortUrl.IntegrationTests.Data;
 using ShortUrl.IntegrationTests.Factories;
 using ShortUrl.IntegrationTests.Options;
 
-namespace ShortUrl.IntegrationTests.Controllers;
+namespace ShortUrl.IntegrationTests.Controllers.v1;
 
 [Collection("Main")]
 public class TokensControllerTests : IClassFixture<UrlShortenerApiFactory> {
@@ -27,7 +24,7 @@ public class TokensControllerTests : IClassFixture<UrlShortenerApiFactory> {
     private async Task CreateShortLink_WithValidUrl_ShouldReturnOk(string url)
     {
         var dto = new CreateShortLinkDto(url);
-        var response = await _client.PostAsJsonAsync("/", dto);
+        var response = await _client.PostAsJsonAsync("api/v1.0/Token", dto);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 
@@ -36,7 +33,7 @@ public class TokensControllerTests : IClassFixture<UrlShortenerApiFactory> {
     private async Task CreateShortLink_WithValidUrl_ShouldReturnValidDto(string url)
     {
         var dto = new CreateShortLinkDto(url);
-        var response = await _client.PostAsJsonAsync("/", dto);
+        var response = await _client.PostAsJsonAsync("api/v1.0/Token", dto);
         var responseDto =
             JsonSerializer.Deserialize<ShortLinkDto>(await response.Content.ReadAsStreamAsync(),
                 JsonWebSerializerOptions.Instance);
@@ -51,7 +48,7 @@ public class TokensControllerTests : IClassFixture<UrlShortenerApiFactory> {
     private async Task CreateShortLink_WithInvalidUrl_ShouldReturnBadRequest(string url)
     {
         var dto = new CreateShortLinkDto(url);
-        var response = await _client.PostAsJsonAsync("/", dto);
+        var response = await _client.PostAsJsonAsync("api/v1.0/Token", dto);
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 
@@ -60,18 +57,18 @@ public class TokensControllerTests : IClassFixture<UrlShortenerApiFactory> {
     private async Task CreateShortLinkThenRedirect_WithValidUrl_ShouldRedirect(string url)
     {
         var dto = new CreateShortLinkDto(url);
-        var response = await _client.PostAsJsonAsync("/", dto);
+        var response = await _client.PostAsJsonAsync("api/v1.0/Token", dto);
         var responseDto =
             JsonSerializer.Deserialize<ShortLinkDto>(await response.Content.ReadAsStreamAsync(),
                 JsonWebSerializerOptions.Instance);
-        var redirectResponse = await _client.GetAsync(responseDto?.Token);
+        var redirectResponse = await _client.GetAsync($"api/v1.0/Token/{responseDto?.Token}");
         redirectResponse.StatusCode.Should().Be(HttpStatusCode.Redirect);
     }
 
     [Fact]
     private async Task Redirect_WithNonExistentToken_ShouldReturn()
     {
-        var response = await _client.GetAsync("-1");
+        var response = await _client.GetAsync("/api/v1.0/Token/-1");
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
